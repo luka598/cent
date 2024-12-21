75111c73069ea88de8f57a4634575202c18bae60
luka598 <luka598@tuta.io>
2024-12-21 13:38:21 +0100

    - more logging

--- src/cent/call/call.py
+++ src/cent/call/call.py
@@ -2,6 +2,9 @@ import typing as T
 from uuid import uuid4
 
 from cent.ether.ws_jsonx import Client
+from cent.logging import Logger
+
+log = Logger(__name__)
 
 
 class CallServer:
@@ -12,8 +15,10 @@ class CallServer:
 
     def register(self, name: str, f: T.Callable) -> None:
         self.funcs[name] = f
+        log.debug(f"Registered {name} for {self.service}")
 
     def start(self) -> None:  # noqa: C901
+        log.debug(f"Started call server for {self.service}")
         while True:
             msg = self.client.recv()
 
@@ -25,30 +30,40 @@ class CallServer:
                 args = msg["args"]
                 no_ret = msg["no_ret"]
             except KeyError:
+                log.debug("Got invalid message")
                 continue
             # ---
             if not isinstance(call_id, bytes):
+                log.debug("Got invalid call_id; not bytes")
                 continue
             if len(call_id) != 16:
+                log.debug("Got invalid call_id; invalid length")
                 continue
 
             if not isinstance(service, str):
+                log.debug("Got invalid service name; not str")
                 continue
             if service != self.service:
+                log.debug("Got invalid service name; mismatch")
                 continue
 
             if not isinstance(func, str):
+                log.debug("Got invalid func name; not str")
                 continue
             if func not in self.funcs:
+                log.debug("Got invalid func name; not registered")
                 continue
 
             if not isinstance(args, dict):
+                log.debug("Got invalid args; not dict")
                 continue
 
             if not isinstance(no_ret, bool):
+                log.debug("Got invalid no_ret; not bool")
                 continue
             # ---
             try:
+                log.debug(f"Got request for {func} with call_id {call_id}")
                 ret = self.funcs[func](**args)
                 if not isinstance(ret, tuple):
                     ret = (ret,)
@@ -61,7 +76,9 @@ class CallServer:
                         "ret": list(ret),
                     }
                 )
+                log.debug(f"Sent response for {func}")
             except Exception as e:
+                log.debug(f"Failed to execute {func} - {e}")
                 if no_ret:
                     continue
                 self.client.send(
@@ -71,6 +88,7 @@ class CallServer:
                         "ret": [e.__class__.__name__, str(e)],
                     }
                 )
+                log.debug(f"Sent error response for {func}")
 
 
 class CallClient:
@@ -82,6 +100,7 @@ class CallClient:
 
     def call(self, service: str, func: str, args: T.Dict) -> T.Tuple:
         call_id = uuid4().bytes
+        log.debug(f"Calling {func} with call_id {call_id}")
         self.client.send(
             {
                 "call_id": call_id,
@@ -91,6 +110,7 @@ class CallClient:
                 "no_ret": False,
             }
         )
+        log.debug(f"Sent request for {func}")
 
         while True:
             msg = self.client.recv()
@@ -100,20 +120,26 @@ class CallClient:
                 success = msg["success"]
                 ret = msg["ret"]
             except KeyError:
+                log.debug("Got invalid message")
                 continue
             # ---
             if not isinstance(call_id, bytes):
+                log.debug("Got invalid call_id; not bytes")
                 continue
             if call_id != ret_call_id:
+                log.debug("Got invalid call_id; mismatch")
                 continue
 
             if not isinstance(success, bool):
+                log.debug("Got invalid success; not bool")
                 continue
 
             if not isinstance(ret, list):
+                log.debug("Got invalid ret; not list")
                 continue
             ret = tuple(ret)
             # ---
+            log.debug(f"Got response for {func} - {success}")
             if success:
                 return ret
             else:
@@ -121,6 +147,7 @@ class CallClient:
 
     def call_noret(self, service: str, func: str, args: T.Dict) -> None:
         call_id = uuid4().bytes
+        log.debug(f"Calling {func} with call_id {call_id}; noret")
         self.client.send(
             {
                 "call_id": call_id,
@@ -130,3 +157,4 @@ class CallClient:
                 "no_ret": True,
             }
         )
+        log.debug(f"Sent request for {func}; noret")
